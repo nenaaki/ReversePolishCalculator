7392a3dbc336481d13350bef22a9aec691d53005
masahiro.morita <masahiro.morita@cct-inc.co.jp>
2022-03-08 19:58:35 +0900

    ICalculatorのファクトリクラスの作成

---
+++ Calculator/CalculatorFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculator
+{
+    /// <summary>
+    /// Calculatorのファクトリクラス
+    /// </summary>
+    public class CalculatorFactory
+    {
+        /// <summary>
+        /// Calculatorを作成する
+        /// </summary>
+        /// <returns></returns>
+        public static ICalculator CreateCalculator()
+            => new Calculator();
+    }
+}
