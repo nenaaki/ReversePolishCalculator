a1edd1f90139f61c1799f9bc1fcade4efd0469a8
nenaaki <nenaaki@gmail.com>
2022-03-14 14:18:02 +0900

    基底クラス作成。軽微なリファクタリング

---
+++ Calculator/RPNComponents/LogicalOperatorBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.RPNComponents
+{
+    internal abstract class LogicalOperatorBase : BasicOperator
+    {
+        protected abstract string Name { get; }
+
+        public override bool TryParse(string token, out ICalculationTarget? result)
+        {
+            result = token == Name ? Create() : null;
+            return result is not null;
+        }
+
+        protected abstract ICalculationTarget Create();
+    }
+}
