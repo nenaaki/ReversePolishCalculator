34cf14af11dad600ecb8bc388c0777b591be5427
nenaaki <nenaaki@gmail.com>
2022-06-15 13:23:44 +0900

    WPF(MVVM)版を作成した

---
+++ RPNCalculator.WPF/DelegateCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace RPNCalculator.WPF
+{
+    internal class DelegateCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private Func<object?, bool>? _canExecute;
+
+        private Action<object?> _execute;
+
+        public DelegateCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object? parameter) => _execute(parameter);
+
+        public void OnCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
