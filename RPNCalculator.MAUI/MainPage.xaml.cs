e2c4ada2d2981608d33c5c4d5964e7eac8d44620
nenaaki <nenaaki@gmail.com>
2022-06-15 14:43:27 +0900

    計算機能の実装

--- RPNCalculator.MAUI/MainPage.xaml.cs
+++ RPNCalculator.MAUI/MainPage.xaml.cs
@@ -6,6 +6,7 @@ public partial class MainPage : ContentPage
 
 	public MainPage()
 	{
+        BindingContext = new RPNViewModel();
 		InitializeComponent();
 	}
 }
--- RPNCalculator.MAUI/RPNViewModel.cs
+++ RPNCalculator.MAUI/RPNViewModel.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
+using Calculator;
 
 namespace RPNCalculator.MAUI
 {
     internal class RPNViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        private ICalculator _calculator = CalculatorFactory.CreateCalculator();
 
         private string _input;
         public string Input {
@@ -40,6 +40,61 @@ namespace RPNCalculator.MAUI
 
         public bool HasError { get => !string.IsNullOrEmpty(_error); }
 
+        public ICommand Push { get; }
 
+        public ICommand Execute { get; }
+
+        public ICommand Pop { get; }
+
+        public ICommand Clear { get; }
+
+        public ObservableCollection<string> Items { get; } = new();
+
+        public RPNViewModel()
+        {
+            _calculator.StackChanged += CalculatorStackChanged;
+
+            Push = new DelegateCommand(_ => {
+                Error = "";
+                try
+                {
+                    _calculator.Push(Input);
+                }
+                catch (Exception e)
+                {
+                    Error = e.Message;
+
+                }
+                Input = "";
+            });
+            Execute = new DelegateCommand(_ => {
+                Error = "";
+                try
+                {
+                    _calculator.Run();
+                }
+                catch (Exception e)
+                {
+                    Error = e.Message;
+
+                }
+            });
+            Pop = new DelegateCommand(_ => {
+                Error = "";
+                Input = _calculator.Pop();
+            });
+            Clear = new DelegateCommand(_ => {
+                Error = "";
+                _calculator.Clean();
+                Input = "";
+            });
+        }
+
+        private void CalculatorStackChanged(object sender, string[] e)
+        {
+            Items.Clear();
+            foreach (var item in e.Where(each => !string.IsNullOrEmpty(each)))
+                Items.Add(item);
+        }
     }
 }
